ef07b56ec12d3efac7cfa29e318d7611b368616f
Adam Baxter <ambaxter@github.com>
2026-03-19 13:22:04 +0000

    Added python docstrings

--- packages/ingest-lib/src/ingest_lib/protocols.py
+++ packages/ingest-lib/src/ingest_lib/protocols.py
@@ -6,20 +6,28 @@ from milvus_lib import ComponentEntry
 
 
 class ProjectExists(Protocol):
+    """Protocol for types that represent a project with a root directory."""
+
     @abstractmethod
     def project_root(self) -> Path:
+        """Return the path to the project root directory."""
         raise NotImplementedError
 
     @abstractmethod
     def project_exists(self) -> bool:
+        """Return True if the project root exists on disk."""
         raise NotImplementedError
 
 
 class ExtractComponents(ProjectExists, Protocol):
+    """Protocol for extractors that yield design system components from a project."""
+
     @abstractmethod
     def component_count(self) -> int:
+        """Return the number of components available in the project."""
         raise NotImplementedError
 
     @abstractmethod
     def extract_components(self) -> Iterator[ComponentEntry]:
+        """Yield ComponentEntry instances from the project."""
         raise NotImplementedError
